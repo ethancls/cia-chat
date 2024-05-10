c56ba6c2354fe507e13bb1f0a19a297d9eb2b89a
ethancls <ethancls@icloud.com>
2024-05-10 23:12:00 +0200

    Automatic commit on Ven 10 mai 2024 23:12:00 CEST

--- src/gtk_offline.c
+++ src/gtk_offline.c
@@ -1,6 +1,72 @@
-#include "./client.h"
+/* Timothée M'BASSIDJE && Ethan NICOLAS -- 2024*/
 
-// gcc -Wall `pkg-config --cflags gtk+-3.0` gtk_offline.c -o cia-chat `pkg-config --libs gtk+-3.0` -lssl -lcrypto
+#pragma once
+#include <gtk/gtk.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h> 
+
+#define NB_LIGNES_INFOS 2
+
+/* Global Variables for GTK */
+GtkWidget *login_window;
+GtkWidget *username_entry;
+GtkWidget *password_entry;
+GtkWidget *login_button;
+GtkWidget *chat_window;
+GtkWidget *chat_view;
+GtkWidget *chat_entry;
+GtkWidget *error_label;
+GtkWidget *signin_window;
+GtkWidget *signin_username_entry;
+GtkWidget *signin_password_entry;
+GtkWidget *signin_firstname_entry;
+GtkWidget *signin_lastname_entry;
+GtkWidget *listbox;
+GtkWidget *conv_name_entry;
+GtkWidget *contact_entry;
+gchar actual_conversation[64];
+gchar user_name[64];
+gboolean update;
+
+/* Function Prototypes */
+void apply_css(GtkWidget *widget, GtkStyleProvider *provider);
+void hash_password(const char *password, char *hashed_password_hex);
+gboolean validate_login(const gchar *username, const gchar *password);
+void login(GtkWidget *widget, gpointer data);
+void send_file(GtkWidget *widget, gpointer data);
+void logout(GtkWidget *widget, gpointer data);
+void on_contact_clicked(GtkWidget *widget, gpointer data);
+gboolean is_contact_valid(const gchar *contact_name);
+gboolean reload_messages(gpointer user_data);
+void start_message_reload_timer();
+void create_new_conversation(GtkWidget *widget, gpointer data);
+void load_contacts_from_file();
+void open_chat_window();
+void to_hex_string(unsigned char *hash, char *output, size_t length);
+void write_login_to_file(const char *username, const char *password);
+void submit_signin(GtkWidget *widget, gpointer data);
+void exit_signin(GtkWidget *widget, gpointer data);
+void signin(GtkWidget *widget, gpointer data);
+void open_login_window();
+int main(int argc, char *argv[]);
+char rand_char();
+void generate_random_id(char *id, size_t size);
+void create_new_conversation_file(const char *conv_name);
+void send_message(GtkWidget *widget, gpointer data);
+void append_to_text_view(const gchar *text);
+gboolean scroll_to_bottom(gpointer data);
+gboolean file_exists(const char *filename);
+gboolean get_conversation_id(const char *user_name, const char *partner_name, char *conversation_id);
+void load_chat_history();
 
 void apply_css(GtkWidget *widget, GtkStyleProvider *provider)
 {
